f13042d01f680218be541f056bcf1beac7dae2cc
maybe-wtudy <han660913@naver.com>
2020-01-03 14:03:53 +0900

    login

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/HomeController")
+@WebServlet("/login")
 public class HomeController extends HttpServlet {
 	private void doProcess(HttpServletRequest request, HttpServletResponse response) {
 		
