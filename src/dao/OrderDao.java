f24a245af612ed6115e14a0b4684cc82f4c5340e
zzidoc94 <zzidoc94@gmail.com>
2020-01-14 11:13:23 +0900

    주문확인 기능 완료

--- src/dao/OrderDao.java
+++ src/dao/OrderDao.java
@@ -31,7 +31,7 @@ public class OrderDao {
 		try {
 			pstmt = con.prepareStatement(sql);
 
-			pstmt.setInt(1, od.getTotcost());
+			pstmt.setInt(1, od.getTototcost());
 			pstmt.setNString(2, od.getAddress());
 			pstmt.setNString(3, od.getBranchid());
 			pstmt.setNString(4, od.getBuyerid());
